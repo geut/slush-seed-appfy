f20861028e23054f37d589309ebbbce85d554b39
Martín Acosta <tinchoz49@gmail.com>
2015-08-20 13:26:25 -0300

    remove tests

--- tests/gulpfile.js
+++
@@ -1,6 +0,0 @@
-/**
- * Gulp tasks to work with browserify, reworkcss and others great tools
- */
-var appfy = require('gulp-appfy-tasks');
-appfy.init(__dirname);
-appfy.defineTasks();
--- tests/src/index.js
+++
@@ -1,6 +0,0 @@
-var $ = require('jquery'),
-    batman = require('batman');
-
-batman();
-
-$('body').append($('<div class="superman"><h2>I\'m superman!!!</h2></div>'));
